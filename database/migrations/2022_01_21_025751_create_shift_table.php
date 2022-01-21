15fa27014a9b902baa8bc98f28bd65dbe248e039
NafhidahRamdhani <cornezaizza25@gmail.com>
2022-01-21 10:05:35 +0700

    Membuat Migration

---
+++ database/migrations/2022_01_21_025751_create_shift_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShiftTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shift', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->time('jam_masuk');
+            $table->time('jam_keluar');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shift');
+    }
+}
